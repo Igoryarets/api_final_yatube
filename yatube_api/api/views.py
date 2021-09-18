f23526aa0ea131dab79d344eb1ff4e83908985cc
Igoryarets <igoryarets338@gmail.com>
2021-09-18 23:31:19 +0300

    последний коммит перед ревью

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import filters, permissions, viewsets
 
-from posts.models import Comment, Group, Post, Follow
+from posts.models import Comment, Follow, Group, Post
 
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
